c3a4ed42b1a8a479bda47a5e69abfdd7a778b994
lucianolorenti <lucianolorenti@gmail.com>
2018-07-12 05:22:50 -0300

    Eredem Sankur Tekalp method

--- src/ImageSegmentationEvaluation.jl
+++ src/ImageSegmentationEvaluation.jl
@@ -1,5 +1,7 @@
 module ImageSegmentationEvaluation
 
 # package code goes here
+include("utils.jl")
 include("supervised.jl")
+include("unsupervised.jl")
 end # module
--- src/unsupervised.jl
+++ src/unsupervised.jl
@@ -23,14 +23,13 @@ function evaluate(c::ECW, image::Matrix{LAB}, segments::Matrix{T}) where T<:Inte
         end
     end
     local E_inter= (2*sum)/(C*prod(size(segmnets)))
-    return 0.5*(E_intra +  E_inter) 
-    
+    return 0.5*(E_intra +  E_inter)   
 end
+
 function evaluate(c::ECW, image::Matrix{RGB}, segments::Matrix{T}) where T<:Integer
     return evaluate(c, LAB.(image), segments)
 end
 
-#Unsupervised Performance Evaluation of Image Segmentation
 
 """
 Zéboudj, Rachid. Filtrage, seuillage automatique, contraste et contours: du pré-traitement à l'analyse d'image. Diss. Saint-Etienne, 1988.
@@ -132,15 +131,23 @@ Multiresolution Color Image Segmentation
 Jianqing Liu and Yee-Hong Yang, Senior Member, IEEE
 
 $F(I) = \sqrt{R} \times \sum\limits_{i=1}^R \dfrac{e_i^2}{\sqrt{A_i}}$
-where $I$ is the image to be segmented, R, the number of regions in the segmented image, $A_i$, the area, or the number of pixels of the ith region $i$, and $e_i$  the color error of region $i$. e is defined as the sum of the Euclidean distance of the color vectors between the original image and the segmented image of each pixel in the region. 
 """
 struct LiuYangF
 end
 
 function scale_factor(c::LiuYangF, image::Matrix, segments::Matrix)
-    return maximum(segments)
+    return sqrt(maximum(segments))
 end
-function evaluate(c::LiuYangF, image::Matrix, segments::Matrix{T}) where T<:Integer
+"""
+´´´julia
+function color_error_sum(image::Matrix, segments::Matrix{Integer)
+´´´
+Computes 
+$\sum\limits_{i=1}^R \dfrac{e_i^2}{\sqrt{A_i}}$
+where $R$ is the number of regions in the segmented image, $A_i$ is the area, or the number of pixels of the ith region $i$, and $e_i$  the color error of region $i$. e is defined as the sum of the Euclidean distance of the color vectors between the original image and the segmented image of each pixel in the region. 
+
+"""
+function color_error_sum(image::Matrix, segments::Matrix{Integer})
     labels = unique(segments)
     out = 0
     for i in labels
@@ -150,13 +157,15 @@ function evaluate(c::LiuYangF, image::Matrix, segments::Matrix{T}) where T<:Inte
         out += e_i/sqrt(A_i)
     end
     return (1/(prod(size(image))))*sqrt(scale_factor(c, image, segments))*out
+end                         
+function evaluate(c::LiuYangF, image::Matrix, segments::Matrix{Integer})
+    return scale_factor(c, image, segments)*color_error_sum(image, segments)
 end
 
 """
-Quantitative evaluation of color image segmentation results 1
+Quantitative evaluation of color image segmentation results 
 M. Borsotti a, P. Campadelli a,2, R. Schettini b,
 """
-
 struct Fprime
 end
 
@@ -167,18 +176,67 @@ function scale_factor(c::FPrime, image::Matrix, segments::Matrix)
         R_A = count(segments_sizes.==seg_size)
         scale = scale + R_A^(1 + (1/A))
     end
-    return scale
+    return (1/prod(size(image))) *scale
 end
 function evaluate(c::FPrime, image::Matrix, segments::Matrix{T}) where T<:Integer
+    return scale_factor(c, image, segments)*color_error_sum(image, segments)
+end
+
+"""
+Quantitative evaluation of color image segmentation results
+M. Borsotti a, P. Campadelli a,2, R. Schettini b,
+
+$Q(I) = \dfrac{1}{10000(N \times M)} \sqrt{R} \times \sum\limits_{i=1}^R \left[  \dfrac{e_i^2}{1+\log A_i} + \left(  \dfrac{R(A_i)}{A_i} \right)^2 \right]$
+
+where $R$ is the number of regions in the segmented image, $A_i$ is the area, or the number of pixels of the ith region $i$, and $e_i$  the color error of region $i$. e is defined as the sum of the Euclidean distance of the color vectors between the original image and the segmented image of each pixel in the region, while $R(A_i)$ represents the number of regions having an area equal to $A_i$.
+"""
+struct Q
+end
+function evaluate(c::Q, image::Matrix, segments::Matrix{Integer})
+    segment_sizes = [count(segments.==i) for i in labels]
     labels = unique(segments)
-    
     out = 0
     for i in labels
         pixels_in_segment = segments.==i
         A_i = count(pixels_in_segment)
         e_i = sum((norm.(mean(image[pixels_in_segment]) - image[pixels_in_segment])).^2)
-        out += e_i/sqrt(A_i)
+        R_A_i = count(segments_sizes.==A_i)
+        out += e_i^2/(1 + log(A_i)) + (R_A_i/A_i)^2
     end
-    return (1/(prod(size(image))))*scale*out
+    return (1/(10000*prod(size(image))))*sqrt(maximum(labels))*out
+end
+
+"""
+Performance Measures for Video Object Segmentation and Tracking
+Erdem, Sankur, Tekalp
+"""
+struct ErdemMethod
+    M::Integer # Neighborhood size to evaluate
+    L::Integer # Length of the normal line
+end
+function mean_value(img::Matrix{T}, pos, w) where T
+    R = CartesianRange(size(img))
+    I, L = first(R), last(R)
+    sum = zeros(T)
+    range = CartesianRange(max(I, pos-w), min(L, pos+w))
+    for N in  range
+        sum += img[N]
+    end
+    return sum / prod(size(range))
+end
+function evaluate(c::AA, image::Matrix, segments::Matrix{Integer})
+    (h,w) = size(image)
+    for i in unique(segments)
+        segment = segments.==i
+    end
+    sum = 0
+    (inside, outside) = normal_lines_extremes(boundary, c.L)
+    for i=1:length(inside)
+        val_inside = mean_value(image, inside[i,:], c.M)
+        val_outside = mean_value(iamge, outside[i,:], c.M)
+        delta_color = norm(val_outside - val_inside)/(sqrt(3*255^2))
+        sum += delta_color
+    end
+    return 1 - (sum/length(inside))
 end
 end
